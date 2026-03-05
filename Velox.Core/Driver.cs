124f26a297101bdaf3c237bab074ce7952152481
luca <luca.rusu@student.unitbv.ro>
2026-03-05 19:42:25 +0200

    Added first versions of entities.

---
+++ Velox.Core/Driver.cs
@@ -0,0 +1,11 @@
+﻿namespace Velox.Core;
+
+public class Driver
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+    public double MaxLoad { get; set; }
+    public List<Order> Orders { get; set; }
+}
---
+++ Velox.Core/Route.cs
@@ -0,0 +1,9 @@
+﻿namespace Velox.Core;
+
+public class Route
+{
+    public string DriverName { get; set; }
+    public Guid Id { get; set; }
+    //route optimizat (probabil json/encoded cumva)
+    
+}
