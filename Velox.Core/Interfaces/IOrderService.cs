9b1218236256ba16385c8981f7fa28171c63c3a7
Vasile Rareș Theodor <theodor.vasile@student.unitbv.ro>
2026-03-05 21:39:01 +0200

    Remaining Core Interfaces for management of Orders and grouping them into repositories.

---
+++ Velox.Core/Interfaces/IOrderService.cs
@@ -0,0 +1,12 @@
+﻿namespace Velox.Core.Interfaces;
+
+public interface IOrderService
+{
+    Task<Order?> GetByIdAsync(Guid orderId);
+    Task<List<Order>> GetAllPendingOrdersAsync();
+    Task<Order> CreateOrderAsync(Order order);
+    Task UpdateOrderStatusAsync(Guid orderId, OrderStatus newStatus);
+    Task<List<Order>> GetOrdersByRouteIdAsync(Guid routeId);
+    Task<List<Order>> GetUnassignedOrdersAsync();
+    Task AssignOrderToRouteAsync(Guid orderId, Guid routeId);
+}
